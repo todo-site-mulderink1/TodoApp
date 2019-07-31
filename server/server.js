653a511a6a2035f638a17f1dcf4a27ef185e4a4f
Michael Mulderink <michaelmulderink@Michaels-MacBook-Pro.local>
2019-07-31 12:58:01 -0700

    Added files ang boilerplate code

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require('express'), path = require('path'), app = express(), PORT = 3434;
+
+app.use('/', express.static(path.resolve(__dirname, '../client/assets')));
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/views/index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}`);
+});
