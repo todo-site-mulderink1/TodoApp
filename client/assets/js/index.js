c8f583e95ac849bb4b75c0411a1b60cd322b577a
Mike Mulderink <mulderink1@gmail.com>
2019-08-14 17:01:13 -0700

    Cleaned up js file

--- client/assets/js/index.js
+++ client/assets/js/index.js
@@ -1,25 +1,26 @@
-function main() {
-  const save = document.getElementById("save");
-  save.addEventListener("click", buildMessage)
-}
+document.getElementById("save").addEventListener("click", buildMessage);
 
 function buildMessage() {
-  let message = document.getElementById("desc").value;
-  if (message === '') return;
-  const htmlNode = document.getElementById("message-list");
-  document.getElementById("desc").value = '';
-  const divNode = document.createElement('div');
-  divNode.setAttribute('class', 'del');
-  const liNode = document.createElement('li');
-  liNode.appendChild(document.createTextNode(message));
-  const button = document.createElement("BUTTON");
-  button.innerHTML = "Delete";
-  button.addEventListener("click", deleteFunc);
-  liNode.appendChild(button);
-  divNode.appendChild(liNode);
-  htmlNode.appendChild(divNode);
-}
+  const message = document.getElementById("desc").value,
+    htmlNode = document.getElementById("message-list"),
+    divNode = document.createElement('div'),
+    liNode = document.createElement('li'),
+    button = document.createElement("BUTTON");
+
+  if (message === '') {
+    return;
+  } else {
+    document.getElementById("desc").value = '';
+    divNode.setAttribute('class', 'del');
+    liNode.appendChild(document.createTextNode(message));
+    button.innerHTML = "Delete";
+    button.addEventListener("click", deleteFunc);
+    liNode.appendChild(button);
+    divNode.appendChild(liNode);
+    htmlNode.appendChild(divNode);
+  };
+};
 
 function deleteFunc() {
-  document.activeElement.parentElement.remove()
-}
+  document.activeElement.parentElement.remove();
+};
